8abc6ab4c2fd7df60bcf1f67d5178dfe86a867ce
august <august@august-sh1tbox>
2025-09-30 16:07:59 -0500

    improved lastAvailable tests

--- tests/lastAvailable.test.ts
+++ tests/lastAvailable.test.ts
@@ -1,35 +1,16 @@
-// import choose_last_available  from "./lastAvailable";
-// import { Player } from "./Board";
-
-
-// test("returns negative one when given empty array", () => {
-//     // let foo0: Player[] = []; // zero
-//     expect(choose_last_available([])).toBe(-1);
-// });
-
-
-// test("returns zero when given array of one element", () => {
-//     const foo = [null] as Player[]; // one
-//     expect(choose_last_available(foo)).toBe(0);
-// });
-
-// const foo2: Player[] = [null, null]; // many
-
-// test("returns one when given array of two elements", () => {
-//     expect(choose_last_available(foo2)).toBe(1);
-// });
-
-// import '@testing-library/jest-dom';
-import choose_last_available from './lastAvailable';
+import choose_last_available from '../components/lastAvailable';
 
 describe('zombies tests on choose_last_available function', () => {
-    it ('returns -1', () => {
-        expect(choose_last_available([])).toBe(-1);
+    it ('returns -1 when given no elements', () => {
+        expect(choose_last_available([])).toBe(-1); // zero
     });
-    it ('returns 0', () => {
-        expect(choose_last_available([null])).toBe(0);
+    it ('returns 0 when given one element', () => {
+        expect(choose_last_available([null])).toBe(0); // one
     });
-    it ('returns 1', () => {
-        expect(choose_last_available([null, null])).toBe(1);
+    it ('returns 1 when given two elements', () => {
+        expect(choose_last_available([null, null])).toBe(1); // many
+    });
+    it ('returns 8 when given 9 elements', () => {
+        expect(choose_last_available(Array(9).fill(null))).toBe(8); // more many
     });
 });
